732013c3e8ab6281258f56c74d0b67e434fb7982
roishoiron <rois.khoiron@gmail.com>
2026-02-25 00:08:36 +0700

    docs: adjust assessment scores and embed inline review comments

--- lib/presentation/providers/trip_provider.dart
+++ lib/presentation/providers/trip_provider.dart
@@ -2,5 +2,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mission_5_wanderly/domain/entities/trip_entity.dart';
 
 final tripListProvider = Provider<List<TripEntity>>((ref) {
+  //{Inline Review: Data trip masih static list; pertimbangkan repository/provider persistence agar scalable.}
   return tripList;
 });
